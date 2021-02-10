e239db3c8ffe3f6a0bf5480622388a27d9e3addc
KitKat420 <danteclark66@gmail.com>
2021-02-10 14:29:02 -0500

    included .env

--- app.ts
+++ app.ts
@@ -1,23 +1,21 @@
 import axios from 'axios';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
 
-enum Token {
-    BEAR_TOKEN = "AAAAAAAAAAAAAAAAAAAAALR5MgEAAAAA9JRi4lVUlnP2th4OJPP3aDcoaak%3Dx3iaVtA96nYnuX5KSh80uYVsUxel8PoQUp3HPfFJ9oQ2zg1hW1"
-};
+
 
 const url = 'https://api.twitter.com';
 
-// axios.defaults.baseURL = url;
-// axios.defaults.headers.common['Authorization'] = Token.BEAR_TOKEN;
-
 const instance = axios.create({
     baseURL: url,
-    headers: {'Authorization': `Bearer ${Token.BEAR_TOKEN}`}
+    headers: {'Authorization': `Bearer ${process.env.BEARER_TOKEN}`}
 
 })
 
 instance.get('/2/users/44196397/tweets')
 .then((response) => {
-    console.log(response.data);
+    const twitterData = response.data;
+    console.log(twitterData.data[0].text);
 })
 .catch((error) => {
     console.log(error)
--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,39 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = __importDefault(require("axios"));
-var Token;
-(function (Token) {
-    Token["BEAR_TOKEN"] = "AAAAAAAAAAAAAAAAAAAAALR5MgEAAAAA9JRi4lVUlnP2th4OJPP3aDcoaak%3Dx3iaVtA96nYnuX5KSh80uYVsUxel8PoQUp3HPfFJ9oQ2zg1hW1";
-})(Token || (Token = {}));
-;
+var dotenv = __importStar(require("dotenv"));
+dotenv.config({ path: __dirname + '/.env' });
 var url = 'https://api.twitter.com';
-// axios.defaults.baseURL = url;
-// axios.defaults.headers.common['Authorization'] = Token.BEAR_TOKEN;
 var instance = axios_1.default.create({
     baseURL: url,
-    headers: { 'Authorization': "Bearer " + Token.BEAR_TOKEN }
+    headers: { 'Authorization': "Bearer " + process.env.BEARER_TOKEN }
 });
 instance.get('/2/users/44196397/tweets')
     .then(function (response) {
-    console.log(response.data);
+    var twitterData = response.data;
+    console.log(twitterData.data[0].text);
 })
     .catch(function (error) {
     console.log(error);
